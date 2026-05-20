f8f39fdd351799c8151e8f89040c93c6e9788655
NamaAnda <email-github-anda@example.com>
2026-05-20 13:08:47 +0800

    fix vercel backend build errors

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,7 +1,10 @@
 import express, { type Express } from "express";
+
 import cors from "cors";
-import helmet from "helmet";
-import pinoHttp from "pino-http";
+import helmet from "helmet"
+app.use(helmet());
+import pinoHttp from "pino-http"
+app.use(pinoHttp());
 import router from "./routes/index.js";
 import { logger } from "./lib/logger.js";
 import { globalLimiter } from "./middlewares/rateLimiter.js";
